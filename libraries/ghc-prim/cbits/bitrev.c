951c45c36f71e71e9889d1a6987b10876413744e
Alexandre Baldé <alexandrer_b@outlook.com>
2019-03-21 16:48:55 +0000

    Add comments explaining bit-handling functions

--- libraries/ghc-prim/cbits/bitrev.c
+++ libraries/ghc-prim/cbits/bitrev.c
@@ -14,6 +14,10 @@ To summarize, the lookup table is faster, but much more memory-heavy e.g.
 doing it for 64-bit words can take 64KB if only 16-bits are reversed at a time.
 Working directly with bits is slower, but uses much less memory as bit-wise
 operators aren't space-onerous.
+
+For more information on how the below bit-twiddling functions came to be, see
+[this](http://graphics.stanford.edu/~seander/bithacks.html#ReverseParallel)
+page.
 */
 
 extern StgWord hs_bitrev8(StgWord x);
@@ -53,11 +57,17 @@ extern StgWord64 hs_bitrev64(StgWord64 x);
 StgWord64
 hs_bitrev64(StgWord64 x)
 {
+  // swap odd and even bits
   x = ((x >> 1)  & 0x5555555555555555) | ((x & 0x5555555555555555) << 1 );
+  // swap consecutive pairs of bits
   x = ((x >> 2)  & 0x3333333333333333) | ((x & 0x3333333333333333) << 2 );
+  // swap consecutive pairs of nibbles (a nibble is 4 bits)
   x = ((x >> 4)  & 0x0F0F0F0F0F0F0F0F) | ((x & 0x0F0F0F0F0F0F0F0F) << 4 );
+  // swap consecutive pairs of bytes
   x = ((x >> 8)  & 0x00FF00FF00FF00FF) | ((x & 0x00FF00FF00FF00FF) << 8 );
+  // swap consecutive pairs of 16-bit words
   x = ((x >> 16) & 0x0000FFFF0000FFFF) | ((x & 0x0000FFFF0000FFFF) << 16);
+  // swap 32-bit long pairs
   x = ( x >> 32                      ) | ( x << 32                      );
   return x;
 }
